810e0f2e42132f45fcfec07de529a79ba43a143b
imkarthi16 <karthiveera@gmail.com>
2022-08-04 10:17:40 +0530

    Exploring the Redux DevTools

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,21 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const uiSlice = createSlice({
-    name : 'ui',
-    initialState : {cartIsVisible : false, notification : null},
-    reducers : {
-        toggle(state){
-            state.cartIsVisible = !state.cartIsVisible
-        },
-        showNotification(state, action){
-           state.notification = {
-            status : action.payload.status,
-            title : action.payload.title,
-            message : action.payload.message
-           };
-        },
+  name: 'ui',
+  initialState: { cartIsVisible: false, notification: null },
+  reducers: {
+    toggle(state) {
+      state.cartIsVisible = !state.cartIsVisible;
     },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
+    },
+  },
 });
 
-export const uiActions= uiSlice.actions;
+export const uiActions = uiSlice.actions;
+
 export default uiSlice;
